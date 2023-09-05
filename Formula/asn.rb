ca0ded6c0fcdd2013b3b375cd208abe6eaac4b1b
Filippo Vitale <filippo.vitale@netscout.com>
2023-09-05 15:13:03 -0400

    upgrade asn to 0.74

--- Formula/asn.rb
+++ Formula/asn.rb
@@ -1,9 +1,9 @@
 class Asn < Formula
   desc "Organization lookup and server tool (ASN / IPv4 / IPv6 / Prefix / AS Path)"
   homepage "https://github.com/nitefood/asn"
-  url "https://raw.githubusercontent.com/nitefood/asn/6cb445632065a43afde84f513ce0ecf12c9d0877/asn"
-  version "0.72.9"
-  sha256 "e34f70f6cb9e6c46991130f924d31dfe8cc62abf39846dca328d52213c07a8bd"
+  url "https://raw.githubusercontent.com/nitefood/asn/a19482aab9a024c7baa425fdd0b899fbd0bfcb09/asn"
+  version "0.74"
+  sha256 "ce4c4775d874fc8574fb503ee40464d2cdda6384d527636c03e1d0eb2adc1223"
   license "MIT"
 
   depends_on "bash"
@@ -28,6 +28,8 @@ class Asn < Formula
   def caveats
     <<~EOS
       To enable the "AS path tracing" feature, MTR should be in $PATH.
+      If a simple brew link doesn't work:
+        brew link mtr
       One option could be to manually add a symbolic link to the MTR executable:
         sudo ln -sfn /usr/local/sbin/mtr /usr/local/bin/mtr
         sudo ln -sfn /usr/local/sbin/mtr-packet /usr/local/bin/mtr-packet
