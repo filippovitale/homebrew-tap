7fa9fcb30aab8abcc89ff9c6190963f5b4c00b99
Filippo Vitale <filippo.vitale@netscout.com>
2023-09-18 15:57:37 -0400

    fix the appcast stanza deprecated warning

--- Casks/mmdbinspect.rb
+++ Casks/mmdbinspect.rb
@@ -3,7 +3,11 @@ cask "mmdbinspect" do
   sha256 "e1a83dc00119bab2101b6d37364065484ca271a68f732a34e87d87b913b693be"
 
   url "https://github.com/maxmind/mmdbinspect/releases/download/v#{version}/mmdbinspect_#{version}_darwin_amd64.tar.gz"
-  appcast "https://github.com/maxmind/mmdbinspect/releases.atom"
+
+  livecheck do
+    url "https://github.com/maxmind/mmdbinspect/releases.atom"
+  end
+
   name "mmdbinspect"
   desc "Look up records for one or more IPs/networks in one or more .mmdb databases"
   homepage "https://github.com/maxmind/mmdbinspect"
